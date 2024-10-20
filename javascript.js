8671fba437a398f0761afce75ca430c60fafeb43
Datsteelfn2 <datsteelfn@gmail.com>
2024-10-19 20:11:13 -0400

    got logic down, all remaining is adding gamefunction and keeping trakc of score

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-console.log("hellow world");
+
+let rock,paper,scissors;
+rock="rock"
+paper="paper"
+scissors="scissors";
 
 function getComputerChoice(){
-    let rock,paper,scissors;
+    
     let num=Math.random();
     
     if(num<=.333){
@@ -16,11 +20,39 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let input=prompt("Pick rock paper or scissors")
-}
+    let input=prompt("Pick rock paper or scissors, write in lower case");
+    return input.toLowerCase();
 
-function PlayRound(humanChoice,computerChoice){
 
 }
 
 const humanSelection=getHumanChoice();
+const computerSelection=getComputerChoice();
+
+function playRound(humanSelection,computerSelection){
+    if(humanSelection==rock && computerSelection==paper){
+        return "Computer wins, you lose";
+    }
+    else if(humanSelection==rock && computerSelection==scissors){
+        return "you win, Computer loses"
+    }
+    else if(humanSelection==paper && computerSelection==rock){
+        return " you win, computer loses"
+    }
+    else if(humanSelection==paper && computerSelection==scissors){
+        return " you lose, computer wins"
+    }
+    else if(humanSelection==scissors && computerSelection==rock){
+        return " you lose, computer wins"
+    }
+    else if(humanSelection==scissors && computerSelection==paper){
+        return " you win, computer loses"
+    }
+    else{
+        return "its a draw";
+    }
+
+    
+}
+
+console.log(playRound(humanSelection,computerSelection));
