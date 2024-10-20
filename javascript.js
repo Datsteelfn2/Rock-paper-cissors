91036a354ba7231b6aad73527f7d85c1ceaa4410
Datsteelfn2 <datsteelfn@gmail.com>
2024-10-19 20:01:17 -0400

    got the logic going on with some if statements and can take user input and result the winner output

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
 console.log("hellow world");
+
+function getComputerChoice(){
+    let rock,paper,scissors;
+    let num=Math.random();
+    
+    if(num<=.333){
+        return rock;
+    }
+    else if(num>=.334&& num<=.667){
+        return paper;
+    }
+    else{
+        return scissors;
+    }
+}
+
+function getHumanChoice(){
+    let input=prompt("Pick rock paper or scissors")
+}
+
+function PlayRound(humanChoice,computerChoice){
+
+}
+
+const humanSelection=getHumanChoice();
