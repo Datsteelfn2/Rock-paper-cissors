a6ee1d640de1bbb8c169ac5b92a126b337b7559e
Datsteelfn2 <datsteelfn@gmail.com>
2024-10-20 09:27:28 -0400

    made the playGame function and managed to put the score out, just need to find a way to play multiple rounds

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 
-let rock,paper,scissors;
+let rock,paper,scissors,humanScore,computerScore;
 rock="rock"
 paper="paper"
 scissors="scissors";
+humanScore=0;
+computerScore=0;
 
 function getComputerChoice(){
     
@@ -25,34 +27,41 @@ function getHumanChoice(){
 
 
 }
+    let humanSelection=getHumanChoice();
+    let computerSelection=getComputerChoice();
+    
 
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
 
 function playRound(humanSelection,computerSelection){
+    
     if(humanSelection==rock && computerSelection==paper){
-        return  `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose`;
+        computerScore++;
+        return  `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose, score|Human:${humanScore} Computer:${computerScore}`;
     }
     else if(humanSelection==rock && computerSelection==scissors){
-        return `You chose ${humanSelection} computer chose ${computerSelection}, you win`
+        humanScore++;
+        return `You chose ${humanSelection} computer chose ${computerSelection}, you win, score|Human:${humanScore} Computer:${computerScore}`
     }
     else if(humanSelection==paper && computerSelection==rock){
-        return `You chose ${humanSelection} computer chose ${computerSelection}, you win`
+        return `You chose ${humanSelection} computer chose ${computerSelection}, you win,score|Human:${humanScore} Computer:${computerScore}`
     }
     else if(humanSelection==paper && computerSelection==scissors){
-        return `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose`;
+        return `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose,score|Human:${humanScore} Computer:${computerScore}`;
     }
     else if(humanSelection==scissors && computerSelection==rock){
-        return `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose`;
+        return `You chose ${humanSelection} computer chose ${computerSelection}, computer wins you lose,score|Human:${humanScore} Computer:${computerScore}`;
     }
     else if(humanSelection==scissors && computerSelection==paper){
-        return `You chose ${humanSelection} computer chose ${computerSelection}, you win`
+        return `You chose ${humanSelection} computer chose ${computerSelection}, you win,score|Human:${humanScore} Computer:${computerScore}`
     }
     else{
-        return "its a draw";
+        return `You chose ${humanSelection} computer chose ${computerSelection}, its a draw,score|Human:${humanScore} Computer:${computerScore}`;
     }
 
     
 }
+function playGame(){
+    return playRound(humanSelection,computerSelection);
 
-console.log(playRound(humanSelection,computerSelection));
+}
+console.log(playGame());
